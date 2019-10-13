5c07822ef5cf549c58c907242065e0d58c8639f7
liangshuai <526995681@qq.com>
2019-10-14 02:48:09 +0800

    1

--- src/components/PagerTest.js
+++ src/components/PagerTest.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Pager from './Pager'
+import StudentList from './StudentList'
+import Modal from './Modal'
 
 export default class PagerTest extends Component {
     constructor(props) {
@@ -8,20 +10,42 @@ export default class PagerTest extends Component {
             current: 1,
             total: 0,
             limit: 10,
-            panelNumber: 5
+            panelNumber: 5,
+            students: [],
+            isLoading: false
         }
-        fetch('/api/')
+        this.fetchStudents();
+
+    }
+    async fetchStudents() {
+        this.setState({
+            isLoading: true
+        })
+        const res = await fetch(`http://open.duyiedu.com/api/student/findByPage?appkey=demo13_1545210570249&page=${this.state.current}&size=${this.state.limit}`)
+            .then(res => res.json())
+            .then(res => res.data)
+        this.setState({
+            total: res.cont,
+            students: res.findByPage,
+            isLoading: false
+        })
     }
     handelPageChange = (newPage) => {
         this.setState({
             current: newPage
         })
+        this.fetchStudents();
     }
     render() {
         return (
-            <div>
-                <Pager {...this.state} onPageChange={this.handelPageChange} />
+            <div className="container">
+                <StudentList stus={this.state.students} />
+                <div>
+                    <Pager {...this.state} onPageChange={this.handelPageChange} />
+                </div>
+                <Modal show={this.state.isLoading} />
             </div>
+
         )
     }
 }
