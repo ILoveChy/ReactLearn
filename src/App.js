ca356aacf249af96a809178ee4cb50f14eb959f5
liangshuai <526995681@qq.com>
2019-10-18 13:48:04 +0800

    蒙层√

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
 import Modal from './components/common/Modal/index'
+import './App.css'
 
 export default class App extends Component {
+    state = {
+        showModal: false
+    }
+    showModal = () => {
+        this.setState({
+            showModal: true
+        })
+    }
+    hideModal = () => {
+        this.setState({
+            showModal: false
+        })
+    }
     render() {
         return (
             <>
-                <Modal show={true} />
+                <img src="https://sta-op.douyucdn.cn/butterfly-java/2019/09/29/eaf922cf0497f5a79d2cb449426e1436.jpg" alt="" />
+                {
+                    this.state.showModal ?
+                        (
+                            <Modal onClose={this.hideModal}>
+                                <div style={{
+                                    background: "#fff"
+                                }}>
+                                    <h1>加载中</h1>
+                                    <button onClick={this.hideModal}>关闭蒙层</button>
+                                </div>
+                            </Modal>
+                        ) : null
+                }
+
+
+                <button onClick={this.showModal} >显示蒙层</button>
             </>
         )
     }
--- src/components/common/Modal/index.js
+++ src/components/common/Modal/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './index.css'
 export default function Modal(props) {
-    if (!props.show) {
-        return null;
-    }
+
+    let defaultProps = {//默认属性
+        bg: "rgba(0,0,0,.5)"
+    };
+    let datas = Object.assign({}, defaultProps, props)
     return (
-        <div className="modal">
-            <p> 加载中</p>
+        <div onClick={e => {
+            if (e.target.className === "modal") {
+                datas.onClose()
+            }
+        }} className="modal" style={{ background: datas.bg }}>
+            <div className="modal-center">
+                {datas.children}
+            </div>
         </div>
     )
 }
