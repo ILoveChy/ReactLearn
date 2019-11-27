f669fcf18fc9b15ca67a7c061f88641af71d3f77
liangshuai <526995681@qq.com>
2019-11-28 00:16:13 +0800

    性能优化 Memo Hook √

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
-import React, { PureComponent, useState, useCallback } from 'react'
+import React, { useState, useMemo } from 'react'
 
-
-
-class Test extends PureComponent {
-  render() {
-    console.log('Test Render');
-
-    return (
-      <div>
-        <h1>{this.props.text}</h1>
-        <button onClick={this.props.onClick}>改变文本</button>
-      </div>
-    )
-  }
-}
-function Parent() {
-  console.log('Parent Render');
-
-  const [text, setText] = useState(1);
-  const [number, setNumber] = useState(0);
-  const handleClick = useCallback(() => {
-    setText(text => text + 1)
-  }, [text])
-  return (
-    <div>
-      {/* 函数的地址每次渲染都发生了变化,导致了子组件跟着重新渲染,若子组件是经过优化的组件,则可能导致优化失效 */}
-      <input value={number} onChange={e => setNumber(parseInt(e.target.value))} type="number" />
-      <Test text={text} onClick={handleClick} />
-      {/* ()=>{} 每次渲染时候Parent的地址都发生了变化  */}
-    </div>
-  )
+function Item(props) {
+  console.log(`Item Render ${props.value}`);
+  return <li>{props.value}</li>
 }
 
 export default function App() {
+  const [range,] = useState({ min: 1, max: 1000 });
+  const [num, setNum] = useState(0);
+  const list = useMemo(() => {
+    const list = [];
+    for (let i = range.min; i <= range.max; i++) {
+      list.push(<Item key={i} value={i}></Item>)
+    }
+    return list
+  }, [range])
+
   return (
     <div>
-      <Parent />
+      <ul>
+        {list}
+      </ul>
+      <input type="number"
+        value={num}
+        onChange={e => { setNum(parseInt(e.target.value)) }}
+      />
     </div>
   )
 }
